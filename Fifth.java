98fe39f53ab29fd482efda59ca48ae920148eb5d
Abhi <madhurapanthulaabhir.5@gmail.com>
2026-06-05 20:03:27 +0530

    files added

---
+++ Fifth.java
@@ -0,0 +1,12 @@
+class Fifth
+{
+	int age=22;
+	String name="Abhiram";
+	
+	public static void main(String[] args)
+	{
+	Fifth obj=new Fifth();
+	System.out.println("My name is " +obj.name+" and i am "+obj.age+" years old");
+}
+}
+
---
+++ Four.java
@@ -0,0 +1,12 @@
+class Four
+{
+	
+	public static void main(String[] args)
+	{
+	int a=10;
+	String name="Abhiram";
+	double b=23.456;
+	System.out.println("The value of a is:" +a+" and b is "+b+" and my name is "+name);
+}
+}
+
