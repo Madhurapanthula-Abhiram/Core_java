7f89beab4f126625a49eb9a50208d36531718096
Abhi <madhurapanthulaabhir.5@gmail.com>
2026-06-05 19:59:30 +0530

    Third

---
+++ Third.java
@@ -0,0 +1,11 @@
+class Third
+{
+	static float b=56.7f;
+	Boolean res=true;
+	public static void main(String[] args)
+	{
+	Third ob=new Third();
+	System.out.println("The value is:" +ob.res+" and "+b);
+}
+}
+
