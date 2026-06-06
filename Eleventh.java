eef90a8ac858fbabd5e3bf99c476ddcaea745641
Abhi <madhurapanthulaabhir.5@gmail.com>
2026-06-06 17:54:27 +0530

    eleven

---
+++ Eleventh.java
@@ -0,0 +1,14 @@
+class Eleventh
+{
+	 void display(boolean value)
+	{
+	System.out.println("It is "+value);
+	}
+	
+	public static void main(String[] args)
+	{
+	Eleventh obj=new Eleventh();
+	obj.display(true);
+}
+}
+
