6645465ac25d3dbecb700d44fb1942d7aaa2fee9
Abhi <madhurapanthulaabhir.5@gmail.com>
2026-06-06 17:51:44 +0530

    Seven

---
+++ Seven.java
@@ -0,0 +1,11 @@
+class Seven
+{
+	static double a=45.6789;
+	
+	public static void main(String[] args)
+	{
+	Boolean a=true;
+	System.out.println("The double value is:"+Seven.a+"\nThe boolean value is :"+a);
+}
+}
+
