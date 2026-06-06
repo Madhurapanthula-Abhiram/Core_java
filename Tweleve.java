b7ad0baf75b4feb84376dccbbe03327448405004
Abhi <madhurapanthulaabhir.5@gmail.com>
2026-06-06 17:55:17 +0530

    tweleve

---
+++ Tweleve.java
@@ -0,0 +1,19 @@
+class Tweleve
+{
+	 void m1(String value)
+	{
+	System.out.println("Hello "+value);
+	}
+	void m2(int value2)
+	{
+	System.out.println("It is "+value2);
+	}
+	
+	public static void main(String[] args)
+	{
+	Tweleve obj=new Tweleve();
+	obj.m1("Kiran");
+	obj.m2(10);
+}
+}
+
