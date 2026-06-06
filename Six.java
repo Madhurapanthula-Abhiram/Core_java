4a5b5bef0b70755ba459ac191d7c51516f5e3bad
Abhi <madhurapanthulaabhir.5@gmail.com>
2026-06-06 17:51:02 +0530

    Six

---
+++ Six.java
@@ -0,0 +1,11 @@
+class Six
+{
+	
+	public static void main(String[] args)
+	{
+	int a=10;
+	int b=5;
+	System.out.println("The sum is:"+(a+b)+"\nThe multiplication is:"+(a*b)+"\nThe division is:"+(a/b));
+}
+}
+
