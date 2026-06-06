37685df7f3d20134ada63a8179d3772cf7cf8e14
Abhi <madhurapanthulaabhir.5@gmail.com>
2026-06-06 17:54:05 +0530

    Ten

---
+++ Ten.java
@@ -0,0 +1,14 @@
+class Ten
+{
+	 void display(String name)
+	{
+	System.out.println("Hello "+name);
+	}
+	
+	public static void main(String[] args)
+	{
+	Ten obj=new Ten();
+	obj.display("Abhiram");
+}
+}
+
