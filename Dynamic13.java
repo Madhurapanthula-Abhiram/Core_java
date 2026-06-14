a059d8738336308c6ffe90dce1d45a1bdcced51f
Madhurapanthula-Abhiram <madhurapanthulaabhir.5@gmail.com>
2026-06-14 12:26:14 +0530

    Add files via upload
    added 3 files

---
+++ Dynamic13.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Dynamic13
+{
+	static Scanner sc=new Scanner(System.in);
+	static String b=sc.next();
+	static void m1(Dynamic13 x)
+	{
+	System.out.println(b);
+	}
+	public static void main(String[] args)
+	{
+	m1(new Dynamic13());
+	}
+	}
+	
